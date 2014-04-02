b4ba1146c334b69711d6779574fa3c5f37b207ea
riiwo <riivoparts@gmail.com>
2014-04-02 20:57:34 +0300

    proov

--- php/footer.php
+++ php/footer.php
@@ -3,4 +3,4 @@
     <div id="footer"><center>Copyright &copy; 2014 Itioot</center></div>
 </body>
 </html>
-?>
+<?php ?>
--- php/header.php
+++ php/header.php
@@ -1,7 +1,7 @@
-<?php
+<?php ?>
 <html>
-<head>
-    <title>Itioot</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
+    <head>
+        <title>Itioot</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
 <body>
